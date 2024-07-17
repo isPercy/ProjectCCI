e6dc2976bcb5f4566d836b242d695da9caa9421c
juan pablo godoy <jpablo.godoyb@gmail.com>
2024-07-17 19:11:27 -0400

    personal-version

--- src/app.js
+++ src/app.js
@@ -1,45 +1,19 @@
-const myconnection = require('express-myconnection');
-const mysql = require('mysql');
 const express = require('express');
-const session = require('express-session');
+const morgan = require('morgan');
 const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
-
-// definicion de conexión a la base de datos
-app.use(myconnection(mysql, {
-   host: 'localhost',
-   user: 'root',
-   password: '',
-   port: 3306,
-   database: 'team_z'
-}, 'single'));
-
-// configuracion de session
-app.use(session({
-	secret: 'secret-string-cci',
-	resave: true,
-	saveUninitialized: true,
-   loggedin: false
-}));
-
-// Configuracion de Body-Parser 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//  Iniciar el servidor
 const port = 4000;
+
 app.listen(port, () => {
-   console.log(`Servidor iniciado http://localhost:${port}`);
+   console.log(`🖥️ 📡 Servidor iniciado http://localhost:${port}`);
 });
 
-//  Configura el motor de plantillas HBS
-app.set('view engine', 'hbs');
-//  Define la ruta para tus archivos de plantilla
-app.set('views', path.join(__dirname, 'views'));
-//  Configura la ubicación de tus archivos estáticos (como CSS y JS)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev')); // tiny, short, common, dev, combined
 
-//  Define la ubicacion de las rutas
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, 'views'));
+
 const Router = require('./routes/rutas');
+
 app.use('/', Router);
